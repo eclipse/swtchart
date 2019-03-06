064ef41b99a86f87015561136f670c63eea1d2d4
Dr. Philip Wenig <philip.wenig@openchrom.net>
2019-03-06 11:21:18 +0100

    Header has been updated.

--- org.eclipse.swtchart.extensions/src/org/eclipse/swtchart/extensions/linecharts/LineChart.java
+++ org.eclipse.swtchart.extensions/src/org/eclipse/swtchart/extensions/linecharts/LineChart.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2017, 2018 Lablicate GmbH.
+ * Copyright (c) 2017, 2019 Lablicate GmbH.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -15,11 +15,11 @@ import java.util.List;
 
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swtchart.ILineSeries;
 import org.eclipse.swtchart.extensions.core.BaseChart;
 import org.eclipse.swtchart.extensions.core.ISeriesData;
 import org.eclipse.swtchart.extensions.core.ScrollableChart;
 import org.eclipse.swtchart.extensions.exceptions.SeriesException;
-import org.eclipse.swtchart.ILineSeries;
 
 public class LineChart extends ScrollableChart {
 
