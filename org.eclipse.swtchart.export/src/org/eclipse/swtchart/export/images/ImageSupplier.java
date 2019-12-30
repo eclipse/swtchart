a4ab0ba46f5c0d32945960fad105a92aee8fa085
frank <frank@macbook-pro-de-frank.home>
2019-12-30 18:10:04 +0100

    https://github.com/eclipse/swtchart/issues/86

--- org.eclipse.swtchart.export/src/org/eclipse/swtchart/export/images/ImageSupplier.java
+++ org.eclipse.swtchart.export/src/org/eclipse/swtchart/export/images/ImageSupplier.java
@@ -15,52 +15,77 @@ package org.eclipse.swtchart.export.images;
 import org.eclipse.swt.graphics.GC;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.ImageData;
+import org.eclipse.swt.graphics.ImageDataProvider;
 import org.eclipse.swt.graphics.ImageLoader;
-import org.eclipse.swt.graphics.Rectangle;
-import org.eclipse.swt.graphics.Transform;
+import org.eclipse.swt.graphics.PaletteData;
 import org.eclipse.swtchart.extensions.core.BaseChart;
 
 public class ImageSupplier {
 
+	/**
+	 * Save image data representation to a file with specific format (BMP, PNG or JPG)
+	 * @param imageData the image data representation
+	 * @param fileName the file (relative or full path)
+	 * @param format SWT.IMAGE_BMP, SWT.IMAGE_PNG or SWT.IMAGE_JPEG 
+	 */
 	public void saveImage(ImageData imageData, String fileName, int format) {
-
 		ImageLoader imageLoader = new ImageLoader();
 		imageLoader.data = new ImageData[]{imageData};
 		imageLoader.save(fileName, format);
 	}
 
+	/**
+	 * Return image data representation of provided chart
+	 * @param baseChart the chart from which is create image data representation
+	 * @return the image data representation
+	 */
 	public ImageData getImageData(BaseChart baseChart) {
-
-		Image image = new Image(baseChart.getDisplay(), baseChart.getBounds());
-		GC gc = new GC(image);
-		/*
-		 * Under macOS, the image is flipped.
-		 */
-		if(isMacOS()) {
-			Transform transform = new Transform(gc.getDevice());
-			transform.rotate(-180);
-			Rectangle bounds = image.getBounds();
-			transform.translate(-bounds.width, -bounds.height);
-			gc.setTransform(transform);
-			baseChart.print(gc);
-			transform.dispose();
-		} else {
-			baseChart.print(gc);
-		}
-		//
+		// Force to redraw chart immediately to be sure that any
+		// previous Shell dialog won't be a part of the copied image
+		baseChart.redraw();
+		baseChart.update();
+		// Chart size
+		int width = baseChart.getSize().x;
+		int height = baseChart.getSize().y;
+		// Get image data provider
+		ImageDataProvider imageDataProvider = getImageDataProvider(width, height);
+		// Copy chart into the image
+		Image image = new Image(baseChart.getDisplay(), imageDataProvider);
+		GC gc = new GC(baseChart);
+		gc.copyArea(image, 0, 0);
+		// Retrieve image data
+		ImageData imageData = image.getImageData(100);
+		// If generated image created from copy area has not same size as chart, scale image data
+		if(width != imageData.width || height != imageData.height) 
+			imageData = resize(image, width, height);
+		// Dispose
 		gc.dispose();
-		ImageData imageData = image.getImageData();
 		image.dispose();
 		return imageData;
 	}
 
-	private boolean isMacOS() {
-
-		return (getOperatingSystem().indexOf("mac") >= 0);
+	// Create image data provider from provided size
+	private ImageDataProvider getImageDataProvider(int width, int height) {
+		// Create the image provider
+		ImageDataProvider imageDataProvider = new ImageDataProvider() {
+			@Override
+			public ImageData getImageData(int zoom) {
+				if(zoom != 100) return null;
+				PaletteData palette = new PaletteData(0xFF, 0xFF00, 0xFF0000);
+				ImageData imageData = new ImageData(width, height, 24, palette);
+				return imageData;
+			}
+		};
+		return imageDataProvider;
 	}
+	
+	// Resize srcImage to width*height
+	private ImageData resize(Image srcImage, int width, int height) {
+		  Image scaledImage = new Image(srcImage.getDevice(), width, height);
+		  GC gc = new GC(scaledImage);
+		  gc.drawImage(srcImage, 0, 0, srcImage.getBounds().width, srcImage.getBounds().height, 0, 0, width, height);
+		  gc.dispose();
+		  return scaledImage.getImageData();
+		}
 
-	private String getOperatingSystem() {
-
-		return System.getProperty("os.name").toLowerCase();
-	}
 }
