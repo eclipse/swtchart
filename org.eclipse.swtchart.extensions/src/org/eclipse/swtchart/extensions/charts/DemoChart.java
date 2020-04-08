82177f259a45ad3f3d9108484aa5b33592bbfeb9
yashTEF <yashsoc@gmail.com>
2020-04-08 14:58:17 +0530

    Demo chart for testing
    Signed-off-by: yashTEF <yashsoc@gmail.com>

---
+++ org.eclipse.swtchart.extensions/src/org/eclipse/swtchart/extensions/charts/DemoChart.java
@@ -0,0 +1,26 @@
+package org.eclipse.swtchart.extensions.charts;
+
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swtchart.extensions.examples.parts.LineSeries_1_Part;
+
+public class DemoChart {
+
+	public static void main(String args[]) {
+
+		Display display = new Display();
+		Shell shell = new Shell(display);
+		shell.setText("Step Chart");
+		shell.setSize(500, 400);
+		shell.setLayout(new FillLayout());
+		LineSeries_1_Part chart = new LineSeries_1_Part(shell);
+		shell.open();
+		while(!shell.isDisposed()) {
+			if(!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+		display.dispose();
+	}
+}
