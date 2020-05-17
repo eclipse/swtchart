c333b676caccbbc8ea87ea7ca6cc0a276f547449
Himanshu Balasamanta <himanshubb.eee18@itbhu.ac.in>
2020-05-17 10:08:37 +0530

    Updated copyright headers
    Signed-off-by: Himanshu Balasamanta <himanshubb.eee18@itbhu.ac.in>

--- org.eclipse.swtchart.extensions.examples/src/org/eclipse/swtchart/extensions/examples/parts/MeasurementSeries_2_Part.java
+++ org.eclipse.swtchart.extensions.examples/src/org/eclipse/swtchart/extensions/examples/parts/MeasurementSeries_2_Part.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2017, 2019 Lablicate GmbH.
+ * Copyright (c) 2017, 2020 Lablicate GmbH.
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
