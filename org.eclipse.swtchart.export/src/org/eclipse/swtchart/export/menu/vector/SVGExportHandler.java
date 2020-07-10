9a1dd13df0a9bb279f970789e13851a2627f8027
yashTEF <yashsoc@gmail.com>
2020-07-10 15:19:56 +0530

    Changes made
    Signed-off-by: yashTEF <yashsoc@gmail.com>

--- org.eclipse.swtchart.export/src/org/eclipse/swtchart/export/menu/vector/SVGExportHandler.java
+++ org.eclipse.swtchart.export/src/org/eclipse/swtchart/export/menu/vector/SVGExportHandler.java
@@ -418,7 +418,7 @@ public class SVGExportHandler extends AbstractSeriesExportHandler implements ISe
 					if(!isReversedX) {
 						x1 = 23.5 + ((axisScaleConverter.convertToSecondaryUnit(value) - xLower) / (xUpper - xLower) * x);
 					} else {
-						x1 = ((23.5 + x) + ((axisScaleConverter.convertToSecondaryUnit(value) - xLower) / (xUpper - xLower) * x));
+						x1 = ((23.5 + x) - ((axisScaleConverter.convertToSecondaryUnit(value) - xLower) / (xUpper - xLower) * x));
 					}
 					ret = String.valueOf(x1);// $NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
 				} else if(axis.equals(AXIS_Y)) {
@@ -749,7 +749,7 @@ public class SVGExportHandler extends AbstractSeriesExportHandler implements ISe
 					if(!isReversedY) {
 						y1 = 80.5 + ((yUpper - axisScaleConverter.convertToSecondaryUnit(value)) / (yUpper - yLower) * y);
 					} else {
-						y1 = ((263.5 - y) + ((yUpper - axisScaleConverter.convertToSecondaryUnit(value)) / (yUpper - yLower) * y));
+						y1 = ((263.5 - y) - ((yUpper - axisScaleConverter.convertToSecondaryUnit(value)) / (yUpper - yLower) * y));
 					}
 					ret = String.valueOf(y1);// $NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
 				}
@@ -983,7 +983,7 @@ public class SVGExportHandler extends AbstractSeriesExportHandler implements ISe
 					if(!isReversedX) {
 						x1 = 23.5 + ((axisScaleConverter.convertToSecondaryUnit(value) - xLower) / (xUpper - xLower) * x);
 					} else {
-						x1 = ((23.5 + x) + ((axisScaleConverter.convertToSecondaryUnit(value) - xLower) / (xUpper - xLower) * x));
+						x1 = ((23.5 + x) - ((axisScaleConverter.convertToSecondaryUnit(value) - xLower) / (xUpper - xLower) * x));
 					}
 					ret = String.valueOf(x1);// $NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
 				} else if(axis.equals(AXIS_Y)) {
