ef50f3c472bd0162620ad85d9056281f6c1edd52
Philip Wenig <philip.wenig@openchrom.net>
2020-12-04 10:58:27 +0100

    Added comments to prevent auto-formatting.

--- org.eclipse.swtchart.extensions/src/org/eclipse/swtchart/extensions/charts/ChartOptions.java
+++ org.eclipse.swtchart.extensions/src/org/eclipse/swtchart/extensions/charts/ChartOptions.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2019 Lablicate GmbH.
+ * Copyright (c) 2019, 2020 Lablicate GmbH.
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -21,43 +21,43 @@ import org.eclipse.swtchart.extensions.linecharts.LineChart;
 
 public class ChartOptions {
 
-	public static String[][] FONT_STYLES = new String[][]{
-			{Messages.getString(Messages.NORMAL), Integer.toString(SWT.NORMAL)}, 
-			{Messages.getString(Messages.BOLD), Integer.toString(SWT.BOLD)}, 
-			{Messages.getString(Messages.ITALIC), Integer.toString(SWT.ITALIC)}
+	public static String[][] FONT_STYLES = new String[][]{ //
+			{Messages.getString(Messages.NORMAL), Integer.toString(SWT.NORMAL)}, //
+			{Messages.getString(Messages.BOLD), Integer.toString(SWT.BOLD)}, //
+			{Messages.getString(Messages.ITALIC), Integer.toString(SWT.ITALIC)} //
 	};
-
-	public static String[][] COMPRESSION_TYPES = new String[][]{
-			{LineChart.COMPRESSION_EXTREME, LineChart.COMPRESSION_EXTREME}, 
-			{LineChart.COMPRESSION_HIGH, LineChart.COMPRESSION_HIGH}, 
-			{LineChart.COMPRESSION_MEDIUM, LineChart.COMPRESSION_MEDIUM}, 
-			{LineChart.COMPRESSION_LOW, LineChart.COMPRESSION_LOW}, {LineChart.COMPRESSION_AUTO, LineChart.COMPRESSION_AUTO}, 
-			{LineChart.COMPRESSION_NONE, LineChart.COMPRESSION_NONE}
+	//
+	public static String[][] COMPRESSION_TYPES = new String[][]{ //
+			{LineChart.COMPRESSION_EXTREME, LineChart.COMPRESSION_EXTREME}, //
+			{LineChart.COMPRESSION_HIGH, LineChart.COMPRESSION_HIGH}, //
+			{LineChart.COMPRESSION_MEDIUM, LineChart.COMPRESSION_MEDIUM}, //
+			{LineChart.COMPRESSION_LOW, LineChart.COMPRESSION_LOW}, {LineChart.COMPRESSION_AUTO, LineChart.COMPRESSION_AUTO}, //
+			{LineChart.COMPRESSION_NONE, LineChart.COMPRESSION_NONE} //
 	};
-
-	public static String[][] SYMBOL_TYPES = new String[][]{
-			{Messages.getString(Messages.NONE), PlotSymbolType.NONE.toString()},
-			{Messages.getString(Messages.CIRCLE), PlotSymbolType.CIRCLE.toString()}, 
-			{Messages.getString(Messages.CROSS), PlotSymbolType.CROSS.toString()},
-			{Messages.getString(Messages.DIAMON), PlotSymbolType.DIAMOND.toString()}, 
-			{Messages.getString(Messages.INVERTED_TRIANGLE), PlotSymbolType.INVERTED_TRIANGLE.toString()}, 
-			{Messages.getString(Messages.PLUS), PlotSymbolType.PLUS.toString()}, 
-			{Messages.getString(Messages.SQUARE), PlotSymbolType.SQUARE.toString()}, 
-			{Messages.getString(Messages.TRIANGLE), PlotSymbolType.TRIANGLE.toString()}, 
-			{Messages.getString(Messages.EMOJI), PlotSymbolType.EMOJI.toString()}
+	//
+	public static String[][] SYMBOL_TYPES = new String[][]{ //
+			{Messages.getString(Messages.NONE), PlotSymbolType.NONE.toString()}, //
+			{Messages.getString(Messages.CIRCLE), PlotSymbolType.CIRCLE.toString()}, //
+			{Messages.getString(Messages.CROSS), PlotSymbolType.CROSS.toString()}, //
+			{Messages.getString(Messages.DIAMON), PlotSymbolType.DIAMOND.toString()}, //
+			{Messages.getString(Messages.INVERTED_TRIANGLE), PlotSymbolType.INVERTED_TRIANGLE.toString()}, //
+			{Messages.getString(Messages.PLUS), PlotSymbolType.PLUS.toString()}, //
+			{Messages.getString(Messages.SQUARE), PlotSymbolType.SQUARE.toString()}, //
+			{Messages.getString(Messages.TRIANGLE), PlotSymbolType.TRIANGLE.toString()}, //
+			{Messages.getString(Messages.EMOJI), PlotSymbolType.EMOJI.toString()} //
 	};
-
-	public static String[][] POSITIONS = new String[][]{
-			{Messages.getString(Messages.PRIMARY), Position.Primary.toString()},
-			{Messages.getString(Messages.SECONDARY), Position.Secondary.toString()}
+	//
+	public static String[][] POSITIONS = new String[][]{ //
+			{Messages.getString(Messages.PRIMARY), Position.Primary.toString()}, //
+			{Messages.getString(Messages.SECONDARY), Position.Secondary.toString()} //
 	};
-
-	public static String[][] LINE_STYLES = new String[][]{
-			{Messages.getString(Messages.NONE), LineStyle.NONE.toString()}, 
-			{"-", LineStyle.DASH.toString()}, 
-			{"-.", LineStyle.DASHDOT.toString()}, 
-			{"-..", LineStyle.DASHDOTDOT.toString()}, 
-			{".", LineStyle.DOT.toString()}, 
-			{Messages.getString(Messages.SOLID), LineStyle.SOLID.toString()}
+	//
+	public static String[][] LINE_STYLES = new String[][]{ //
+			{Messages.getString(Messages.NONE), LineStyle.NONE.toString()}, //
+			{"-", LineStyle.DASH.toString()}, //
+			{"-.", LineStyle.DASHDOT.toString()}, //
+			{"-..", LineStyle.DASHDOTDOT.toString()}, //
+			{".", LineStyle.DOT.toString()}, //
+			{Messages.getString(Messages.SOLID), LineStyle.SOLID.toString()} //
 	};
 }
