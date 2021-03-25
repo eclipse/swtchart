ff4f5e821f40c772d8ca99a0aee4a01ae93fab5e
Philip Wenig <philip.wenig@openchrom.net>
2021-03-25 08:59:39 +0100

    Fixed #244 - Adjust Preference Page Title

--- org.eclipse.swtchart.extensions/src/org/eclipse/swtchart/extensions/preferences/PreferencePage.java
+++ org.eclipse.swtchart.extensions/src/org/eclipse/swtchart/extensions/preferences/PreferencePage.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2020 Lablicate GmbH.
+ * Copyright (c) 2020, 2021 Lablicate GmbH.
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -24,12 +24,14 @@ import org.eclipse.ui.IWorkbenchPreferencePage;
 public class PreferencePage extends FieldEditorPreferencePage implements IWorkbenchPreferencePage {
 
 	public PreferencePage() {
+
 		super(GRID);
 		IPreferenceStore preferenceStore = ResourceSupport.getPreferenceStore();
 		if(preferenceStore != null) {
 			setPreferenceStore(preferenceStore);
 		}
-		setTitle("SWTChart Extensions");
+		//
+		setTitle("SWTChart");
 		setDescription("");
 	}
 
