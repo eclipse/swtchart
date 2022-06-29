f2f4efb8696517170cd5ecf4dd94073b309d0ce9
Philip Wenig <philip.wenig@openchrom.net>
2022-06-29 16:38:05 +0200

    An example for a fixed range chart has been created.

---
+++ org.eclipse.swtchart.extensions.examples/src/org/eclipse/swtchart/extensions/examples/charts/FixedRangeChart.java
@@ -0,0 +1,51 @@
+/*******************************************************************************
+ * Copyright (c) 2022 SWTChart project.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ * Philip Wenig - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.swtchart.extensions.examples.charts;
+
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swtchart.IAxis;
+import org.eclipse.swtchart.Range;
+import org.eclipse.swtchart.extensions.core.BaseChart;
+import org.eclipse.swtchart.extensions.core.ScrollableChart;
+import org.eclipse.swtchart.extensions.examples.parts.BarSeries_1_1_Part;
+
+public class FixedRangeChart {
+
+	public static void main(String args[]) {
+
+		Display display = new Display();
+		Shell shell = new Shell(display);
+		shell.setText("FixedRangeChart");
+		shell.setSize(500, 400);
+		shell.setLayout(new FillLayout());
+		//
+		ScrollableChart scrollableChart = new BarSeries_1_1_Part(shell);
+		shell.open();
+		/*
+		 * Set a fixed range.
+		 */
+		IAxis axisX = scrollableChart.getBaseChart().getAxisSet().getXAxis(BaseChart.ID_PRIMARY_X_AXIS);
+		axisX.setRange(new Range(10, 250));
+		IAxis axisY = scrollableChart.getBaseChart().getAxisSet().getYAxis(BaseChart.ID_PRIMARY_Y_AXIS);
+		axisY.setRange(new Range(0, 1500));
+		//
+		while(!shell.isDisposed()) {
+			if(!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+		display.dispose();
+	}
+}
