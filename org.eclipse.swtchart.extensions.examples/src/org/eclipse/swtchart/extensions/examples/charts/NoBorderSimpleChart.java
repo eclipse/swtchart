083c5c5a460ba7fb3961c223d6bdd9cbdfe5d9f5
Philip Wenig <philip.wenig@openchrom.net>
2022-08-23 06:07:19 +0200

    An example to display a chart without borders has been added.

---
+++ org.eclipse.swtchart.extensions.examples/src/org/eclipse/swtchart/extensions/examples/charts/NoBorderSimpleChart.java
@@ -0,0 +1,76 @@
+/*******************************************************************************
+ * Copyright (c) 2022 Lablicate GmbH.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ * 
+ * Contributors:
+ * Philip Wenig - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.swtchart.extensions.examples.charts;
+
+import java.util.List;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swtchart.LineStyle;
+import org.eclipse.swtchart.extensions.core.IChartSettings;
+import org.eclipse.swtchart.extensions.core.ISecondaryAxisSettings;
+import org.eclipse.swtchart.extensions.core.ScrollableChart;
+import org.eclipse.swtchart.extensions.examples.parts.PeakSeries_1_Part;
+
+public class NoBorderSimpleChart {
+
+	public static void main(String args[]) {
+
+		Display display = new Display();
+		Shell shell = new Shell(display);
+		shell.setText("NoBorderSimpleChart");
+		shell.setSize(300, 300);
+		shell.setLayout(new FillLayout());
+		//
+		ScrollableChart scrollableChart = new PeakSeries_1_Part(shell);
+		shell.open();
+		/*
+		 * Use this demo to test the buffer selection in different
+		 * environments.
+		 */
+		Font font = new Font(Display.getCurrent(), "Arial", 12, SWT.NONE);
+		IChartSettings chartSettings = scrollableChart.getChartSettings();
+		chartSettings.setBufferSelection(true);
+		chartSettings.setHorizontalSliderVisible(false);
+		chartSettings.setVerticalSliderVisible(false);
+		chartSettings.getPrimaryAxisSettingsX().setVisible(false);
+		chartSettings.getPrimaryAxisSettingsY().setVisible(false);
+		chartSettings.setTitle("Toluene");
+		chartSettings.setTitleColor(Display.getDefault().getSystemColor(SWT.COLOR_BLACK));
+		chartSettings.setTitleFont(font);
+		chartSettings.setTitleVisible(true);
+		disableSecondaryAxes(chartSettings.getSecondaryAxisSettingsListX());
+		disableSecondaryAxes(chartSettings.getSecondaryAxisSettingsListY());
+		scrollableChart.applySettings(chartSettings);
+		//
+		while(!shell.isDisposed()) {
+			if(!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+		//
+		font.dispose();
+		display.dispose();
+	}
+
+	private static void disableSecondaryAxes(List<ISecondaryAxisSettings> secondaryAxisSettings) {
+
+		for(ISecondaryAxisSettings secondaryAxisSetting : secondaryAxisSettings) {
+			secondaryAxisSetting.setVisible(false);
+			secondaryAxisSetting.setGridLineStyle(LineStyle.NONE);
+		}
+	}
+}
---
+++ org.eclipse.swtchart.extensions.examples/src/org/eclipse/swtchart/extensions/examples/parts/PeakSeries_1_Part.java
@@ -0,0 +1,73 @@
+/*******************************************************************************
+ * Copyright (c) 2022 Lablicate GmbH.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ * 
+ * Contributors:
+ * Philip Wenig - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.swtchart.extensions.examples.parts;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swtchart.ILineSeries.PlotSymbolType;
+import org.eclipse.swtchart.customcharts.core.ChromatogramChart;
+import org.eclipse.swtchart.extensions.core.IChartSettings;
+import org.eclipse.swtchart.extensions.core.ISeriesData;
+import org.eclipse.swtchart.extensions.examples.support.SeriesConverter;
+import org.eclipse.swtchart.extensions.linecharts.ILineSeriesData;
+import org.eclipse.swtchart.extensions.linecharts.ILineSeriesSettings;
+import org.eclipse.swtchart.extensions.linecharts.LineSeriesData;
+
+public class PeakSeries_1_Part extends ChromatogramChart {
+
+	@Inject
+	public PeakSeries_1_Part(Composite parent) {
+
+		super(parent, SWT.NONE);
+		setBackground(getDisplay().getSystemColor(SWT.COLOR_WHITE));
+		//
+		try {
+			initialize();
+		} catch(Exception e) {
+			System.out.println(e);
+		}
+	}
+
+	private void initialize() throws Exception {
+
+		/*
+		 * Chart Settings
+		 */
+		IChartSettings chartSettings = getChartSettings();
+		chartSettings.setCreateMenu(true);
+		applySettings(chartSettings);
+		/*
+		 * Create series.
+		 */
+		List<ILineSeriesData> lineSeriesDataList = new ArrayList<ILineSeriesData>();
+		/*
+		 * Peak
+		 */
+		ISeriesData seriesData = SeriesConverter.getSeriesXY(SeriesConverter.LINE_SERIES_1_SELECTED_PEAK_1);
+		ILineSeriesData lineSeriesData = new LineSeriesData(seriesData);
+		ILineSeriesSettings lineSeriesSettings = lineSeriesData.getSettings();
+		lineSeriesSettings.setEnableArea(true);
+		lineSeriesSettings.setSymbolType(PlotSymbolType.NONE);
+		lineSeriesSettings.setLineColor(getDisplay().getSystemColor(SWT.COLOR_RED));
+		lineSeriesDataList.add(lineSeriesData);
+		/*
+		 * Set series.
+		 */
+		addSeriesData(lineSeriesDataList);
+	}
+}
