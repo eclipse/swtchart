1aae7408420561ef61a11888dc272886dac478bf
Philip Wenig <philip.wenig@openchrom.net>
2023-11-22 09:32:14 +0100

    Fixed #377 - Custom method to get/set status of "Buffered Selection"

--- org.eclipse.swtchart.extensions/src/org/eclipse/swtchart/extensions/preferences/PreferenceSupport.java
+++ org.eclipse.swtchart.extensions/src/org/eclipse/swtchart/extensions/preferences/PreferenceSupport.java
@@ -8,83 +8,104 @@
  * SPDX-License-Identifier: EPL-2.0
  * 
  * Contributors:
- * Dr. Philip Wenig - initial API and implementation
+ * Philip Wenig - initial API and implementation
  * Frank Buloup - Internationalization
  *******************************************************************************/
 package org.eclipse.swtchart.extensions.preferences;
 
 import java.util.Locale;
 
+import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.swt.SWT;
 import org.eclipse.swtchart.IAxis.Position;
 import org.eclipse.swtchart.IBarSeries.BarWidthStyle;
 import org.eclipse.swtchart.ILineSeries.PlotSymbolType;
 import org.eclipse.swtchart.LineStyle;
 import org.eclipse.swtchart.extensions.core.RangeRestriction;
+import org.eclipse.swtchart.extensions.core.ResourceSupport;
 
 public class PreferenceSupport {
 
-	public static String[][] LEGEND_POSITIONS = new String[][]{//
-			{Messages.getString(Messages.LEFT), Integer.toString(SWT.LEFT)}, 
-			{Messages.getString(Messages.RIGHT), Integer.toString(SWT.RIGHT)}, 
-			{Messages.getString(Messages.TOP), Integer.toString(SWT.TOP)}, 
-			{Messages.getString(Messages.BOTTOM), Integer.toString(SWT.BOTTOM)}
+	public static final String[][] LEGEND_POSITIONS = new String[][]{ //
+			{Messages.getString(Messages.LEFT), Integer.toString(SWT.LEFT)}, //
+			{Messages.getString(Messages.RIGHT), Integer.toString(SWT.RIGHT)}, //
+			{Messages.getString(Messages.TOP), Integer.toString(SWT.TOP)}, //
+			{Messages.getString(Messages.BOTTOM), Integer.toString(SWT.BOTTOM)} //
 	};
 	//
-	public static String[][] ORIENTATIONS = new String[][]{//
-			{Messages.getString(Messages.HORIZONTAL), Integer.toString(SWT.HORIZONTAL)}, 
-			{Messages.getString(Messages.VERTICAL), Integer.toString(SWT.VERTICAL)}
+	public static final String[][] ORIENTATIONS = new String[][]{ //
+			{Messages.getString(Messages.HORIZONTAL), Integer.toString(SWT.HORIZONTAL)}, //
+			{Messages.getString(Messages.VERTICAL), Integer.toString(SWT.VERTICAL)} //
 	};
 	//
-	public static String[][] AXIS_POSITIONS = new String[][]{//
-			{Messages.getString(Messages.PRIMARY), Position.Primary.toString()}, 
-			{Messages.getString(Messages.SECONDARY), Position.Secondary.toString()}
+	public static final String[][] AXIS_POSITIONS = new String[][]{ //
+			{Messages.getString(Messages.PRIMARY), Position.Primary.toString()}, //
+			{Messages.getString(Messages.SECONDARY), Position.Secondary.toString()} //
 	};
 	//
-	public static String[][] LOCALES = new String[][]{//
-			{Messages.getString(Messages.ENGLISH), Locale.ENGLISH.getLanguage()}, 
-			{Messages.getString(Messages.US), Locale.US.getLanguage()}, 
-			{Messages.getString(Messages.GERMAN), Locale.GERMAN.getLanguage()}
+	public static final String[][] LOCALES = new String[][]{ //
+			{Messages.getString(Messages.ENGLISH), Locale.ENGLISH.getLanguage()}, //
+			{Messages.getString(Messages.US), Locale.US.getLanguage()}, //
+			{Messages.getString(Messages.GERMAN), Locale.GERMAN.getLanguage()} //
 	};
 	//
-	public static String[][] LINE_STYLES = new String[][]{//
-			{Messages.getString(Messages.NONE), LineStyle.NONE.toString()}, 
-			{Messages.getString(Messages.SOLID), LineStyle.SOLID.toString()}, 
-			{Messages.getString(Messages.DASH), LineStyle.DASH.toString()}, 
-			{Messages.getString(Messages.DOT), LineStyle.DOT.toString()}, 
-			{Messages.getString(Messages.DASH_DOT), LineStyle.DASHDOT.toString()}, 
-			{Messages.getString(Messages.DASH_DOT_DOT), LineStyle.DASHDOTDOT.toString()}
+	public static final String[][] LINE_STYLES = new String[][]{ //
+			{Messages.getString(Messages.NONE), LineStyle.NONE.toString()}, //
+			{Messages.getString(Messages.SOLID), LineStyle.SOLID.toString()}, //
+			{Messages.getString(Messages.DASH), LineStyle.DASH.toString()}, //
+			{Messages.getString(Messages.DOT), LineStyle.DOT.toString()}, //
+			{Messages.getString(Messages.DASH_DOT), LineStyle.DASHDOT.toString()}, //
+			{Messages.getString(Messages.DASH_DOT_DOT), LineStyle.DASHDOTDOT.toString()} //
 	};
 	//
-	public static String[][] ANTIALIAS_OPTIONS = new String[][]{//
-			{Messages.getString(Messages.DEFAULT), Integer.toString(SWT.DEFAULT)}, 
-			{Messages.getString(Messages.ON), Integer.toString(SWT.ON)}, 
-			{Messages.getString(Messages.OFF), Integer.toString(SWT.OFF)}
+	public static final String[][] ANTIALIAS_OPTIONS = new String[][]{ //
+			{Messages.getString(Messages.DEFAULT), Integer.toString(SWT.DEFAULT)}, //
+			{Messages.getString(Messages.ON), Integer.toString(SWT.ON)}, //
+			{Messages.getString(Messages.OFF), Integer.toString(SWT.OFF)} //
 	};
 	//
-	public static String[][] SYMBOL_TYPES = new String[][]{//
-			{Messages.getString(Messages.NONE), PlotSymbolType.NONE.toString()}, 
-			{Messages.getString(Messages.CIRCLE), PlotSymbolType.CIRCLE.toString()}, 
-			{Messages.getString(Messages.CROSS), PlotSymbolType.CROSS.toString()}, 
-			{Messages.getString(Messages.DIAMOND), PlotSymbolType.DIAMOND.toString()}, 
-			{Messages.getString(Messages.INVERTED_TRIANGLE), PlotSymbolType.INVERTED_TRIANGLE.toString()}, 
-			{Messages.getString(Messages.PLUS), PlotSymbolType.PLUS.toString()}, 
-			{Messages.getString(Messages.SQUARE), PlotSymbolType.SQUARE.toString()}, 
-			{Messages.getString(Messages.TRIANGLE), PlotSymbolType.TRIANGLE.toString()}, 
-			{Messages.getString(Messages.EMOJI), PlotSymbolType.EMOJI.toString()}
+	public static final String[][] SYMBOL_TYPES = new String[][]{//
+			{Messages.getString(Messages.NONE), PlotSymbolType.NONE.toString()}, //
+			{Messages.getString(Messages.CIRCLE), PlotSymbolType.CIRCLE.toString()}, //
+			{Messages.getString(Messages.CROSS), PlotSymbolType.CROSS.toString()}, //
+			{Messages.getString(Messages.DIAMOND), PlotSymbolType.DIAMOND.toString()}, //
+			{Messages.getString(Messages.INVERTED_TRIANGLE), PlotSymbolType.INVERTED_TRIANGLE.toString()}, //
+			{Messages.getString(Messages.PLUS), PlotSymbolType.PLUS.toString()}, //
+			{Messages.getString(Messages.SQUARE), PlotSymbolType.SQUARE.toString()}, //
+			{Messages.getString(Messages.TRIANGLE), PlotSymbolType.TRIANGLE.toString()}, //
+			{Messages.getString(Messages.EMOJI), PlotSymbolType.EMOJI.toString()} //
 	};
 	//
-	public static String[][] BAR_WIDTH_STYLES = new String[][]{//
-			{Messages.getString(Messages.FIXED), BarWidthStyle.FIXED.toString()}, 
-			{Messages.getString(Messages.STRETCHED), BarWidthStyle.STRETCHED.toString()}
+	public static final String[][] BAR_WIDTH_STYLES = new String[][]{//
+			{Messages.getString(Messages.FIXED), BarWidthStyle.FIXED.toString()}, //
+			{Messages.getString(Messages.STRETCHED), BarWidthStyle.STRETCHED.toString()} //
 	};
 	//
-	public static String[][] EXTEND_TYPES = new String[][]{//
-			{Messages.getString(Messages.RELATIVE), RangeRestriction.ExtendType.RELATIVE.toString()}, 
-			{Messages.getString(Messages.ABSOLUTE), RangeRestriction.ExtendType.ABSOLUTE.toString()}
+	public static final String[][] EXTEND_TYPES = new String[][]{//
+			{Messages.getString(Messages.RELATIVE), RangeRestriction.ExtendType.RELATIVE.toString()}, //
+			{Messages.getString(Messages.ABSOLUTE), RangeRestriction.ExtendType.ABSOLUTE.toString()} //
 	};
 
 	private PreferenceSupport() {
 
+	}
+
+	public static boolean isBufferedSelection() {
+
+		boolean bufferedSelection = false;
+		IPreferenceStore preferenceStore = ResourceSupport.getPreferenceStore();
+		if(preferenceStore != null) {
+			bufferedSelection = preferenceStore.getBoolean(PreferenceConstants.P_BUFFER_SELECTION);
+		}
+		//
+		return bufferedSelection;
+	}
+
+	public static void setBufferedSelection(boolean active) {
+
+		IPreferenceStore preferenceStore = ResourceSupport.getPreferenceStore();
+		if(preferenceStore != null) {
+			preferenceStore.setValue(PreferenceConstants.P_BUFFER_SELECTION, active);
+		}
 	}
 }
